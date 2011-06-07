8f82e761925bd3838d93cee1577e5a8b6a5464e0
Manuel Korfmann <manu@korfmann.info>
2011-06-07 17:59:09 +0200

    fixing capital typo

--- questions/regex/lazy.rb
+++ questions/regex/lazy.rb
@@ -5,7 +5,7 @@
     "text": "Which regex will capture everything between the square brackets: '[a,b,c]'",
     "a1": "/[(.*)]/",
     "a2": "/[(.+)]/",
-    "a3": "/[.*?]/",
+    "a3": "/[(.*?)]/",
     "right_answer": "a3",
     "created_at": "2011-06-07",
     "created_by": "mkorfmann"
