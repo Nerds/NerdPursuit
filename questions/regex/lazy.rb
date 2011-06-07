7d4960d4bd14f9f49699642582dd9f43220d4f36
Manuel Korfmann <manu@korfmann.info>
2011-06-07 17:38:17 +0200

    adding regex quoestions

---
+++ questions/regex/lazy.rb
@@ -0,0 +1,13 @@
+{ "question" : {
+    "id": "",
+    "category": "regex",
+    "nerd_level": 6,
+    "text": "Which regex will capture everything between the square brackets: '[a,b,c]'",
+    "a1": "/[(.*)]/",
+    "a2": "/[(.+)]/",
+    "a3": "/[.*?]/",
+    "right_answer": "a3",
+    "created_at": "2011-06-07",
+    "created_by": "mkorfmann"
+  }
+}
