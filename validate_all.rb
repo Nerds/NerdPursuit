7f495887299eb5d2e0400c7b3fca266d185343ad
Manuel Korfmann <manu@korfmann.info>
2011-06-10 00:39:27 +0800

    added name of creator to failing tests

--- validate_all.rb
+++ validate_all.rb
@@ -15,7 +15,7 @@ describe 'question' do
         question = File.open(file) { |f| JSON.parse(f.read) }['question']
         JSON::Validator.validate!(schema, question, version: :draft3 )
       rescue JSON::Schema::ValidationError => schema_error
-         assertion =  false, schema_error.message
+         assertion =  false, schema_error.message + "\n[#{question['created_by']}, please fix this and send another pull request!]"
       rescue JSON::ParserError => parser_error
          assertion =  false, parser_error.message
       else
