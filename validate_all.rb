6f29c42352f7782f599712042eebeeec77814396
Georg Leciejewski <gl@salesking.eu>
2011-06-22 11:12:58 +0200

    ensure validate_all is running in ruby 1.8.x

--- validate_all.rb
+++ validate_all.rb
@@ -13,7 +13,9 @@ describe 'question' do
 
 
       begin
-        question = File.open(file) { |f| JSON.parse(f.read.force_encoding('UTF-8')) }['question']
+        json_str = File.open(file) { |f|  f.read }
+        # force_encoding only available in ruby 1.9.2 
+        question = JSON.parse( json_str.respond_to?(:force_encoding ) ? json_str.force_encoding('UTF-8') : json_str)['question']
         JSON::Validator.validate!(schema, question, :version=> :draft3 )
       rescue JSON::Schema::ValidationError => schema_error
          assertion =  false, schema_error.message + "\nIn: #{file}\n[#{question['created_by']}, please fix this and send another pull request!]"
